5672e819c1eed9f06c163c178f7b2840dda3f235
developer-glareus <developer.glareus@gmail.com>
2022-10-10 11:46:04 +0530

    order testing completed

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
-import { Order } from '../../models/orders';
-
 
 it('Fetch the order', async () => {
   const ticket = Ticket.build({
@@ -17,9 +15,32 @@ it('Fetch the order', async () => {
     .send({ ticketId: ticket.id })
     .expect(201)
 
-  await request(app)
+  const { body: fetchOrder } = await request(app)
     .get(`/api/orders/${order.id}`)
     .set('Cookie', user)
     .send()
     .expect(200)
+  expect(fetchOrder.id).toEqual(order.id)
+})
+
+
+
+it('error if one user try to fetch other user ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  })
+  await ticket.save()
+  const user = global.signin()
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401)
 })
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@jhticketss/common';
+import { Order, OrderStatus } from '../models/orders';
 
 const Router = express.Router()
 
 
-Router.delete('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send('data form ')
+Router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
+  const { orderId } = req.params
+  const order = await Order.findById(orderId)
+  if (!order) {
+    throw new NotFoundError()
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+  order.status = OrderStatus.Cancelled
+  await order.save()
+  res.status(204).send(order)
 })
 
 
